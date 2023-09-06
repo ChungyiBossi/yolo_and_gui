b17cdb3cd50eed58ae5f4db56c17b520adf18e33
ChungyiBossi <chungyi.bossi@gmail.com>
2023-09-06 12:25:54 +0800

    [feature] remove unnecessary function & download image

--- analyze_image.py
+++ analyze_image.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import numpy as np
 import mediapipe as mp
-from tkinter import Variable, filedialog
+from tkinter import filedialog
 from PIL import Image, ImageTk
 from mediapipe_object_detection import MPObjectDetectorWrapper
 
@@ -35,32 +35,41 @@ def show_image(img, canvas):
 
 def openfile_and_process(raw_canvas, cooked_canvas, textbox):
     # openfile
-    img_path = filedialog.askopenfilename(filetypes=[('png', '*.png'),('jpg', '*.jpg'),('gif', '*.gif')])  # 指定開啟檔案格式
-    img = Image.open(img_path)           # 依照圖片路徑取得圖片檔
-    show_image(img, raw_canvas)
-    # process
-    numpy_img = np.array(img)
-    detection_result = object_detector.object_detect(numpy_img)
-    annotated_image = object_detector.visualize(numpy_img, detection_result)
-    show_image(Image.fromarray(annotated_image), cooked_canvas)
-    # print result
-    # 整理辨識結果到text box
-    results = list()
-    for result in detection_result.detections:
-        category = result.categories[0]
-        category_name = category.category_name
-        probability = round(category.score, 2)
-        result_text = category_name + ' (' + str(probability) + ')'
-        results.append(result_text)
-    result_for_text_box = "\n".join(results)
-    textbox.delete(1.0,'end')
-    textbox.insert(tk.END, result_for_text_box)
+    img_path = filedialog.askopenfilename(filetypes=[('png', '*.png'),('jpg', '*.jpg'),('gif', '*.gif')])  # 指定開啟檔案格式, 回傳一個檔案路徑
+    if img_path.split('.')[-1] not in ['jps', 'png', '.gif'] and img_path:
+        textbox.delete(1.0,'end')
+        textbox.insert(tk.END, "Wrong file type.")
+    elif img_path != "":
+        img = Image.open(img_path)           # 依照圖片路徑取得圖片檔
+        show_image(img, raw_canvas)
+        # process
+        numpy_img = np.array(img)
+        detection_result = object_detector.object_detect(numpy_img)
+        annotated_image = object_detector.visualize(numpy_img, detection_result)
+        show_image(Image.fromarray(annotated_image), cooked_canvas)
+        # 整理辨識結果到text box
+        results = list()
+        for result in detection_result.detections:
+            category = result.categories[0]
+            category_name = category.category_name
+            probability = round(category.score, 2)
+            result_text = category_name + ' (' + str(probability) + ')'
+            results.append(result_text)
+        result_for_text_box = "\n".join(results)
+        textbox.delete(1.0,'end')
+        textbox.insert(tk.END, result_for_text_box)
 
+def download_processed_image(cooked_canvas):
+    # imgtk is an ImageTk.PhotoImage object
+    img_tk = cooked_canvas.tk_img
+    img = ImageTk.getimage(img_tk)
+    img_save_path = filedialog.asksaveasfilename(filetypes=[('png', '*.png'),('jpg', '*.jpg'),('gif', '*.gif')])
+    img.save(img_save_path)
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Process Image')
-    root.geometry('1600x900')
+    root.geometry('1200x900')
 
     # 創造兩個子視窗 raw/cooked image_frame, 用left frame 包起來
     left_frame = tk.Frame(root)
@@ -78,7 +87,7 @@ if __name__ == '__main__':
     # 創造一個顯示結果的文字框, 包在right frame 之下
     result_text = tk.Text(right_frame, width=800, font=('Arial',20,'bold'),)
     result_text.insert(tk.END, "這裡是空白的")
-    result_text.pack(padx=10, pady=10, anchor='nw')
+    result_text.pack(padx=10, pady=10)
     # result_text.pack(padx=10, pady=10)
     # 上傳圖檔按鈕, 包在 right frame
     button = tk.Button(
@@ -87,7 +96,16 @@ if __name__ == '__main__':
         font=('Arial',15,'bold'),
         command=lambda :openfile_and_process(raw_canvas, cooked_canvas, result_text)
     )
-    button.pack(padx=10, pady=10, side='left')
+    button.pack(padx=10, pady=5, fill='x')
+
+    # 下載圖檔按鈕, 包在 right frame
+    button = tk.Button(
+        right_frame, 
+        text='Download Processed image', 
+        font=('Arial',15,'bold'),
+        command=lambda :download_processed_image(cooked_canvas)
+    )
+    button.pack(padx=10, pady=5, fill='x')
     
     
 
